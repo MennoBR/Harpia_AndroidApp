d5c878a0ccd11c96d97382ec04eadb83d92177bd
MennoBR <malburgcine@hotmail.com>
2023-02-09 21:15:29 -0300

    Harpia1stcom

---
+++ APIClimaNOW.py
@@ -0,0 +1,45 @@
+# API básica de monitoramento do clima na região de Blumenau.
+
+ 
+import requests
+import datetime as dt
+
+#Criando função para definir Kelvin como padrão:
+
+def Padrao_Kelvin(kelvin):
+    celsius = kelvin - 273.15
+    fahrenheit = celsius * (9/5) * 32
+    return celsius, fahrenheit
+
+
+# Obtendo informações:
+
+BASE_URL =  "http://api.openweathermap.org/data/2.5/weather?"
+API_KEY = "6c72ca7e4174d6429033197dabb040ca"
+CITY = "São Paulo"
+url = BASE_URL + "appid=" + API_KEY + "&q=" + CITY
+
+
+# Definindo as conversões de temp e respostas:
+
+resposta = requests.get(url).json()
+temp_kelvin = resposta['main']['temp']
+temp_celsius, temp_fahrenheit = Padrao_Kelvin(temp_kelvin)
+sensacao_kelvin = resposta['main']['feels_like']
+sensacao_celsius, sensacao_fahrenheit = Padrao_Kelvin(sensacao_kelvin)
+umidade = resposta['main']['humidity']
+descricao = resposta['weather'][0]['description']
+vento_velo = resposta['wind']['speed']
+nascer_sol = dt.datetime.utcfromtimestamp(resposta['sys']['sunrise'] + resposta['timezone'])
+por_do_sol = dt.datetime.utcfromtimestamp(resposta['sys']['sunset'] + resposta['timezone'])
+
+
+# Mostrando as informações ao Usuário:
+
+print(f'Temperatura em {CITY}: {temp_celsius:.2f}C ou {temp_fahrenheit:.2f}F.')
+print(f'Sensação térmica em {CITY} é de: {sensacao_celsius:.2f}C ou {sensacao_fahrenheit:.2f}F.')
+print(f'Umidade relativa do ar: {umidade}%.')
+print(f'Velocidade do vento em {CITY}: {vento_velo}km/h.')
+print(f'Formação de Nuvens: {descricao}.')
+print(f'O sol nasce em {CITY}: {nascer_sol} hora local.')
+print(f'O sol se põe em {CITY}: {por_do_sol} hora local.')
---
+++ AviatorMain.py
@@ -0,0 +1,45 @@
+
+import APIClimaNOW
+from kivy.app import App
+from kivy.uix.gridlayout import GridLayout
+from kivy.uix.label import Label
+from kivy.uix.image import Image
+from kivy.graphics import Color, Rectangle
+from kivy.uix.button import Button
+from kivy.uix.textinput import TextInput
+from kivy.core.window import Window
+
+
+class Harpia(App):
+    def build(self):
+        self.window = GridLayout()
+        self.window.cols = 1
+        self.window.size_hint = (0.6, 0.7)
+        self.window.pos_hint = {'center_x':0.5, "center_y":0.5}
+
+
+        with self.window.canvas.before:
+             Color(0.960784, 0.921569, 0.803922)
+             self.rect = Rectangle(size=Window.size, pos=self.window.pos)
+
+
+        self.window.add_widget(Image(source="Harpialogo.jpeg"))
+        self.greeting = Label(text="Quem é você?", font_size = 18, color='green')
+        self.window.add_widget(self.greeting)
+        self.user = TextInput(multiline=False, padding_y=(20, 20), size_hint=(1, 0.5),
+                              background_color='white')
+        self.window.add_widget(self.user)
+        self.button = Button(text='ENTRAR', size_hint=(1, 0.5), bold=True,
+                             background_color='green', background_normal='',)
+        self.button.bind(on_press=self.pressiona)
+        self.window.add_widget(self.button)
+
+        return self.window
+
+    def pressiona(self, evento):
+        self.greeting.text = "Olá Comandante " + self.user.text + '!'
+
+
+
+if __name__ == "__main__":
+    Harpia().run()
