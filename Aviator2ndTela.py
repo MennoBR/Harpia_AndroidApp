d4527001b80a63439d64859e6e5f3c3ba3cf3603
MennoBR <malburgcine@hotmail.com>
2023-02-14 15:26:35 -0300

    finaltercarna

--- Aviator2ndTela.py
+++ Aviator2ndTela.py
@@ -1,4 +1,3 @@
-
 # Segunda tela do APP: Mostrará informações metereológicas ao usuário da cidade escolhida.
 # Será acoplada com as informações da Api já criada "ClimaNOW"
 
@@ -14,19 +13,23 @@ class SegundaTela(GridLayout):
         super(SegundaTela, self).__init__(**kwargs)
         self.cols = 1
         self.add_widget(Label(text="Informações do Clima na Rota:", font_size=20, color='green'))
-        self.add_widget(Label(text="", size_hint_y=None, height=100, color=[1, 1, 1, 1]))
-
-
         atual = APIClimaNOW.get_atual("São Paulo")
-
-        info_box = BoxLayout(orientation='vertical', padding=[20, 20, 20, 20], size_hint_y=None, height=400,
-                             pos_hint={"center_x": 0.5, "center_y": 0.5})
-        with info_box.canvas:
-            Color(1, 1, 1, 1)
-            Rectangle(pos=info_box.pos, size=info_box.size, )
+        info_box = BoxLayout(orientation='vertical', padding=[20, 20, 20, 20], size_hint_y=None, height=400)
         info_box.add_widget(Label(text=f"Cidade: {atual['city']}", font_size=15, color='green'))
         info_box.add_widget(Label(text=f"Temperatura: {atual['temperature']:.2f} °C", font_size=15, color='green'))
         info_box.add_widget(Label(text=f"Umidade: {atual['humidity']}%", font_size=15, color='green'))
         info_box.add_widget(Label(text=f"Descrição: {atual['description']}", font_size=15, color='green'))
         info_box.add_widget(Label(text=f"Velocidade do vento: {atual['wind_speed']}", font_size=15, color='green'))
         self.add_widget(info_box)
+
+        with self.canvas:
+            Color(1, 1, 1, 1)
+            Rectangle(pos=self.pos, size=self.size)
+
+        self.bind(pos=self.update_rect, size=self.update_rect)
+
+    def update_rect(self, *args):
+        self.canvas.before.clear()
+        with self.canvas.before:
+            Color(1, 1, 1, 1)
+            Rectangle(pos=self.pos, size=self.size)
--- rascunho.py
+++ rascunho.py
@@ -1,18 +1,35 @@
 # Rascunho / Testes:
 
-'''with info_box.canvas:
-    Color(1, 1, 1, 1)
-    self.rect = Rectangle(pos=self.pos, size=self.size)
-    self.bind(pos=self.update_rect, size=self.update_rect)
+#Aviator2ndTela.py funcional VR 1:
 
-def update_rect(self, *args):
-    self.rect.pos = self.pos
-    self.rect.size = self.size
+'''# Segunda tela do APP: Mostrará informações metereológicas ao usuário da cidade escolhida.
+# Será acoplada com as informações da Api já criada "ClimaNOW"
 
-self.add_widget(info_box)
-self.add_widget(Label(text="", size_hint_y=None, height=100))
-self.add_widget(Label(text="", size_hint_y=None, height=100))
+import APIClimaNOW
+from kivy.uix.gridlayout import GridLayout
+from kivy.uix.label import Label
+from kivy.uix.boxlayout import BoxLayout
+from kivy.graphics import Color, Rectangle
 
-def on_size(self, *args):
-    self.rect.size = self.size
-    self.rect.pos = (self.width / 2 - self.rect.size[0] / 2, self.height / 2 - self.rect.size[1] / 2)'''
+
+class SegundaTela(GridLayout):
+    def __init__(self, **kwargs):
+        super(SegundaTela, self).__init__(**kwargs)
+        self.cols = 1
+        self.add_widget(Label(text="Informações do Clima na Rota:", font_size=20, color='green'))
+        self.add_widget(Label(text="", size_hint_y=None, height=100, color=[1, 1, 1, 1]))
+
+
+        atual = APIClimaNOW.get_atual("São Paulo")
+
+        info_box = BoxLayout(orientation='vertical', padding=[20, 20, 20, 20], size_hint_y=None, height=400,
+                             pos_hint={"center_x": 0.5, "center_y": 0.5})
+        with info_box.canvas:
+            Color(1, 1, 1, 1)
+            Rectangle(pos=info_box.pos, size=info_box.size, )
+        info_box.add_widget(Label(text=f"Cidade: {atual['city']}", font_size=15, color='green'))
+        info_box.add_widget(Label(text=f"Temperatura: {atual['temperature']:.2f} °C", font_size=15, color='green'))
+        info_box.add_widget(Label(text=f"Umidade: {atual['humidity']}%", font_size=15, color='green'))
+        info_box.add_widget(Label(text=f"Descrição: {atual['description']}", font_size=15, color='green'))
+        info_box.add_widget(Label(text=f"Velocidade do vento: {atual['wind_speed']}", font_size=15, color='green'))
+        self.add_widget(info_box)'''
