7d4191fa9a02e4e250e237100004f98b2c850f62
MennoBR <malburgcine@hotmail.com>
2023-02-14 14:05:48 -0300

    carnaterçacmt

---
+++ apirdmet.py
@@ -0,0 +1,6 @@
+# API oficial com dados oficiais de aviação da rede Redemet,
+# para compilar e fazer o display no App.
+
+'''Token API key: xzvjAbr90ReXv02lZa8zWV4DPqzoNW1eaIiIopkF
+Http reader: curl -H 'X-Api-Key: xzvjAbr90ReXv02lZa8zWV4DPqzoNW1eaIiIopkF' 'https://api-redemet.decea.gov.br/{url}'
+Get Query Param: curl 'https://api-redemet.decea.gov.br?api_key='xzvjAbr90ReXv02lZa8zWV4DPqzoNW1eaIiIopkF'''
