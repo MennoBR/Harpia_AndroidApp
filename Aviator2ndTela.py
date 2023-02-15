651978a887fe3d2eb9d8e5bc61936aeafa642785
MennoBR <malburgcine@hotmail.com>
2023-02-15 19:54:57 -0300

    enlouquecendo

--- Aviator2ndTela.py
+++ Aviator2ndTela.py
@@ -4,11 +4,61 @@
 import APIClimaNOW
 from kivy.uix.gridlayout import GridLayout
 from kivy.uix.label import Label
-from kivy.uix.boxlayout import BoxLayout
 from kivy.graphics import Color, Rectangle
 from kivy.uix.image import Image
 from kivy.uix.floatlayout import FloatLayout
+from kivy.core.window import Window
 
+
+Window.size = (800, 600)
+
+
+Window.minimum_width, Window.minimum_height = Window.size
+
+class SegundaTela(GridLayout):
+    def __init__(self, **kwargs):
+        super(SegundaTela, self).__init__(**kwargs)
+        self.cols = 1
+        self.add_widget(Label(text="Informações do Clima na Rota:", font_size=20, color='green'))
+        atual = APIClimaNOW.get_atual("São Paulo")
+        info_box = GridLayout(cols=2, padding=[20, 20, 20, 20], size_hint_y=None, height=400)
+        info_box.add_widget(Label(text=f"Cidade:"))
+        info_box.add_widget(Label(text=f"{atual['city']}", color='green'))
+        info_box.add_widget(Label(text=f"Temperatura:"))
+        info_box.add_widget(Label(text=f"{atual['temperature']:.2f} °C", color='green'))
+        info_box.add_widget(Label(text=f"Umidade:"))
+        info_box.add_widget(Label(text=f"{atual['humidity']}%", color='green'))
+        info_box.add_widget(Label(text=f"Descrição:"))
+        info_box.add_widget(Label(text=f"{atual['description']}", color='green'))
+        info_box.add_widget(Label(text=f"Velocidade do vento:"))
+        info_box.add_widget(Label(text=f"{atual['wind_speed']}", color='green'))
+        self.add_widget(info_box)
+
+
+class RootWidget(FloatLayout):
+    def __init__(self, **kwargs):
+        super(RootWidget, self).__init__(**kwargs)
+
+        harpia_logo = Image(source="Harpialogo.jpeg", size_hint=(50, 50), pos_hint={'x': 10, 'top': 0})
+        self.add_widget(harpia_logo)
+
+        with self.canvas:
+            Color(0, 1, 0, 1)
+            self.rect = Rectangle(pos=self.pos, size=self.size)
+
+        self.bind(pos=self.update_rect, size=self.update_rect)
+
+        segunda_tela = SegundaTela(pos_hint={'x': 0, 'y': 0}, size_hint=(1, 1))
+        self.add_widget(segunda_tela)
+
+    def update_rect(self, *args):
+        self.rect.pos = self.pos
+        self.rect.size = self.size
+
+
+
+
+#código antigo:
 
 '''class SegundaTela(GridLayout):
     def __init__(self, **kwargs):
@@ -42,46 +92,5 @@ from kivy.uix.floatlayout import FloatLayout
         with self.canvas.before:
             Color(1, 1, 1, 1)
             Rectangle(pos=self.pos, size=self.size)'''
-
-class SegundaTela(GridLayout):
-    def __init__(self, **kwargs):
-        super(SegundaTela, self).__init__(**kwargs)
-        self.cols = 1
-        self.add_widget(Label(text="Informações do Clima na Rota:", font_size=20, color='green'))
-        atual = APIClimaNOW.get_atual("São Paulo")
-        info_box = GridLayout(cols=2, padding=[20, 20, 20, 20], size_hint_y=None, height=400)
-        info_box.add_widget(Label(text=f"Cidade:"))
-        info_box.add_widget(Label(text=f"{atual['city']}", color='green'))
-        info_box.add_widget(Label(text=f"Temperatura:"))
-        info_box.add_widget(Label(text=f"{atual['temperature']:.2f} °C", color='green'))
-        info_box.add_widget(Label(text=f"Umidade:"))
-        info_box.add_widget(Label(text=f"{atual['humidity']}%", color='green'))
-        info_box.add_widget(Label(text=f"Descrição:"))
-        info_box.add_widget(Label(text=f"{atual['description']}", color='green'))
-        info_box.add_widget(Label(text=f"Velocidade do vento:"))
-        info_box.add_widget(Label(text=f"{atual['wind_speed']}", color='green'))
-        self.add_widget(info_box)
-
-
-class RootWidget(FloatLayout):
-    def __init__(self, **kwargs):
-        super(RootWidget, self).__init__(**kwargs)
-
-        harpia_logo = Image(source="Harpialogo.jpeg", size_hint=(20, 20), pos_hint={'x': 0, 'top': 1})
-        self.add_widget(harpia_logo)
-
-        with self.canvas:
-            Color(1, 1, 1, 1)
-            self.rect = Rectangle(pos=self.pos, size=self.size)
-
-        self.bind(pos=self.update_rect, size=self.update_rect)
-
-        segunda_tela = SegundaTela(pos_hint={'x': 0, 'y': 0}, size_hint=(1, 1))
-        self.add_widget(segunda_tela)
-
-    def update_rect(self, *args):
-        self.rect.pos = self.pos
-        self.rect.size = self.size
-
 
 
--- appmenu.py
+++ appmenu.py
@@ -1,2 +1,32 @@
 # Menu do ap com 5 botões para ser linkado com o main e a 2a tela.
 
+from kivy.app import App
+from kivy.uix.gridlayout import GridLayout
+from kivy.uix.button import Button
+from kivy.uix.image import Image
+from kivy.graphics import Color, Rectangle
+
+class MainMenuScreen(GridLayout):
+    def __init__(self, **kwargs):
+        super(MainMenuScreen, self).__init__(**kwargs)
+        self.cols = 2
+
+
+        with self.canvas.before:
+            Color(0.270, 0.345, 0.227, 1)
+            self.rect = Rectangle(size=self.size, pos=self.pos)
+        self.add_widget(Image(source="Harpialogo.jpeg", size_hint=(1, None), height=150))
+
+        # Add buttons
+        self.add_widget(Button(text='Button 1', background_color=(0.423, 0.486, 0.305, 1), background_normal=''))
+        self.add_widget(Button(text='Button 2', background_color=(0.423, 0.486, 0.305, 1), background_normal=''))
+        self.add_widget(Button(text='Button 3', background_color=(0.423, 0.486, 0.305, 1), background_normal=''))
+        self.add_widget(Button(text='Button 4', background_color=(0.423, 0.486, 0.305, 1), background_normal=''))
+        self.add_widget(Button(text='Button 5', background_color=(0.423, 0.486, 0.305, 1), background_normal=''))
+
+class Harpia(App):
+    def build(self):
+        return MainMenuScreen()
+
+if __name__ == "__main__":
+    Harpia().run()
