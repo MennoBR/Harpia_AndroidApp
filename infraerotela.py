b05129105e68c583f23a2f6c54868e982ed6e655
MennoBR <malburgcine@hotmail.com>
2023-02-19 15:35:04 -0300

    dom2ndcmtfim

--- infraerotela.py
+++ infraerotela.py
@@ -1,38 +1,72 @@
-#Tela para mostrar ao usuário informações dos aeroportos.
+#Tela para mostrar ao usuário informações dos aeroportos da API apinfraero.py
 
-from kivy.uix.gridlayout import GridLayout
-from kivy.uix.label import Label
-from kivy.graphics import Color, Rectangle
-from kivy.uix.image import Image
-from kivy.uix.floatlayout import FloatLayout
-from kivy.core.window import Window
+from kivy.app import App
 from kivy.config import Config
+from kivy.uix.floatlayout import FloatLayout
+from kivy.uix.image import Image
+from kivy.graphics import Color, Rectangle
+from kivy.uix.button import Button
+from kivy.uix.label import Label
 
 
-# Travando a janela:
+# Configurando a janela:
+Config.set('graphics', 'resizable', 0)
 Config.set('graphics', 'width', '400')
 Config.set('graphics', 'height', '600')
 
-#Começando build:
-class infrap(FloatLayout):
 
+class MainMenuScreen(FloatLayout):
     def __init__(self, **kwargs):
-        super(infrap, self).__init__(**kwargs)
+        super(MainMenuScreen, self).__init__(**kwargs)
 
         with self.canvas.before:
             Color(0.960784, 0.921569, 0.803922)
             self.rect = Rectangle(size=self.size)
             self.bind(size=self._update_rect)
 
-        #Add e maniplando as imagens:
+        # Adicionando o logo e a imagem
         self.add_widget(
-                Image(source="Harpialogo.jpeg", size_hint=(None, None), size=(150, 75),
-                      pos_hint={'x': 0.32, 'top': 0.95}))
+            Image(source="Harpialogo.jpeg", size_hint=(None, None), size=(200, 100), pos_hint={'x': 0.25, 'top': 0.9}))
         self.add_widget(
-            Image(source="infraero1.png", size_hint=(None, None), size=(300, 150),
-                  pos_hint={'center': 1, 'top': 0}))
+            Image(source="fotobranca.jpg", size_hint=(None, None), size=(90, 40), pos_hint={'x': 0.79, 'top': 1}))
+        self.add_widget(Image(source="infraero1.png", size_hint_y=None, height=200, pos_hint={'x': 0, 'top': 0.6}))
 
+        button_height = 0.07
+        button_padding = 10
+        self.add_widget(
+            Button(text='Voltar', size_hint=(0.3, button_height), bold=True, background_color="green",
+                   background_normal='', pos_hint={'x': 0.35, 'y': 0.15},
+                   border=(9, 9, 9, 9), padding=(button_padding, button_padding)))
+
+        self.add_widget(Label(text='Selecione o Aeroporto e espere carregar:', font_size='18sp', color='green',
+                              pos_hint={'x': 0, 'y': 0.15}, font_name='Roboto-Bold.ttf'))
+
+
+    def _update_rect(self, instance, value):
+        self.rect.pos = instance.pos
+        self.rect.size = instance.size
+
+
+class Harpia(App):
+    def build(self):
+        return MainMenuScreen()
 
 
 if __name__ == "__main__":
-    infrap().run()
+    Harpia().run()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
