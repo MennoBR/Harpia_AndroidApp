efe084e231ba8858f62d12652ff253e4d23a1237
MennoBR <malburgcine@hotmail.com>
2023-02-21 15:30:18 -0300

    only210223cmt

--- apirdmet.py
+++ apirdmet.py
@@ -1,54 +1,59 @@
 # API oficial com dados oficiais de aviação da rede Redemet,
 # para compilar e fazer o display no App.
 
-
 import requests
-import os
 from kivy.app import App
-from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.label import Label
+from kivy.uix.floatlayout import FloatLayout
+from kivy.config import Config
 
-API_KEY = 'xzvjAbr90ReXv02lZa8zWV4DPqzoNW1eaIiIopkF'
+# Travando a janela:
+Config.set('graphics', 'width', '400')
+Config.set('graphics', 'height', '600')
 
-class Redemet(App):
-
+class RedemetApp(App):
     def build(self):
-        layout = BoxLayout(orientation='vertical')
+        # create the layout
+        layout = FloatLayout()
 
-        # Add a label to display the weather data
-        self.weather_label = Label(text="")
+        # Pegando as informações do clima da API:
+        url = "http://api.redemet.aer.mil.br/api/v1/meteograma/prev/24/prognostico/80568?fields=temperature,relativeHumidity,windDirection,windSpeed,pressure,precipitation&api_key=xzvjAbr90ReXv02lZa8zWV4DPqzoNW1eaIiIopkF"
+        try:
+            response = requests.get(url)
+            response.raise_for_status()
+            data = response.json()
 
-        # Add the label to the layout
-        layout.add_widget(self.weather_label)
+            # Mostrando os dados:
+            temp = data["temperature"][0]["value"]
+            temp_label = Label(text=f"Temperatura: {temp}°C", font_size=20, pos_hint={"x": 0.05, "top": 0.9})
+            layout.add_widget(temp_label)
+
+            humidity = data["relativeHumidity"][0]["value"]
+            humidity_label = Label(text=f"\nHumidade: {humidity}%", font_size=20, pos_hint={"x": 0.05, "top": 0.8})
+            layout.add_widget(humidity_label)
+
+            wind_dir = data["windDirection"][0]["value"]
+            wind_speed = data["windSpeed"][0]["value"]
+            wind_label = Label(text=f"\nVentos: {wind_dir} {wind_speed}km/h", font_size=20,
+                               pos_hint={"x": 0.05, "top": 0.7})
+            layout.add_widget(wind_label)
+
+            pressure = data["pressure"][0]["value"]
+            pressure_label = Label(text=f"\nPressão: {pressure}hPa", font_size=20, pos_hint={"x": 0.05, "top": 0.6})
+            layout.add_widget(pressure_label)
+
+            precipitation = data["precipitation"][0]["value"]
+            precipitation_label = Label(text=f"\nPrecipitação: {precipitation}mm", font_size=20,
+                                        pos_hint={"x": 0.05, "top": 0.5})
+            layout.add_widget(precipitation_label)
+
+        except requests.exceptions.RequestException as e:
+            # handle exceptions and display an error message
+            error_label = Label(text=f"Erro: {str(e)}", font_size=20, pos_hint={"x": 0.05, "top": 0.9})
+            layout.add_widget(error_label)
 
         return layout
 
-    def on_start(self):
-        # Retrieve the weather data
-        icao_code = "SBSP"
-        url = 'https://api-redemet.decea.gov.br/mg/{}'.format(icao_code)
-        headers = {'X-Api-Key': API_KEY}
-        response = requests.get(url, headers=headers)
 
-        if response.status_code == 200:
-            data = response.json()
-            clima = {
-                'icao_code': data['local'],
-                'temperature': data['temperatura'],
-                'dew_point': data['temperatura_ponto_orvalho'],
-                'wind_direction': data['direcao_vento'],
-                'wind_speed': data['velocidade_vento'],
-                'pressure': data['pressao_atm'],
-                'humidity': data['umidade_rel']
-            }
-
-
-            self.weather_label.text = f"Código ICAO : {clima['icao_code']}\nTemperatura: {clima['temperature']}°C" \
-                                      f"\nPonto orvalho: {clima['dew_point']}°C\nDireção vento: {clima['wind_direction']}°" \
-                                      f"\nVelocidade vento: {clima['wind_speed']} kts\nPressão: {clima['pressure']}" \
-                                      f" hPa\nHumidade: {clima['humidity']}%"
-        else:
-            self.weather_label.text = 'Erro: Não foi possível acessar os dados'
-
-if __name__ == '__main__':
-    Redemet().run()
+if __name__ == "__main__":
+    RedemetApp().run()
